bfff656402b3245ab38ad29884411ca5b4552135
jaelee <jaewonlee9642@gmail.com>
2025-09-24 16:31:13 -0700

    additional unnecessary import

--- agentic_doc/utils.py
+++ agentic_doc/utils.py
@@ -4,7 +4,6 @@ from collections import defaultdict
 from pathlib import Path
 from typing import Literal, Union, Optional
 from urllib.parse import urlparse
-from typing import Optional, Union
 
 import cv2
 import httpx
